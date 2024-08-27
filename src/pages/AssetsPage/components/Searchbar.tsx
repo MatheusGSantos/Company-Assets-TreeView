5e1fa21870b6a172e8be934d3f7c9baac2e79efd
MatheusGSantos <mths.gm@gmail.com>
2024-08-26 21:22:11 -0300

    refactor: Improve searchbar UI and functionality

--- src/pages/AssetsPage/components/Searchbar.tsx
+++ src/pages/AssetsPage/components/Searchbar.tsx
@@ -7,14 +7,14 @@ interface SearchbarProps {
 
 export function Searchbar({ placeholder, onChange }: Readonly<SearchbarProps>) {
   return (
-    <div className="border border-gray-light rounded-sm flex py-2 pr-3">
+    <div className="border border-gray-light rounded-sm flex items-center py-2 pr-3">
       <input
         type="text"
         placeholder={placeholder}
         onChange={(event) => onChange(event.target.value)}
         className="placeholder:text-placeholder placeholder:font-inter placeholder:font-normal font-inter font-normal text-sm text-blue-dark bg-transparent outline-none py-[5px] px-3 flex-1"
       />
-      <img src={SearchIcon} alt="Search" />
+      <SearchIcon />
     </div>
   );
 }
